ed3dc06d3a99a6658590ad4bd68b9695c94cac4a
Sandro Munda <sandro@munda.me>
2026-04-16 15:59:47 +0200

    revert: remove react-router-dom from @rootcx/ui (deferred to feat/router-ui-sidebar)

--- crates/rootcx-scaffold/templates/scaffold/main.tsx
+++ crates/rootcx-scaffold/templates/scaffold/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { RuntimeProvider } from "@rootcx/sdk";
 import { ThemeProvider } from "@rootcx/ui";
 import "./globals.css";
@@ -8,12 +7,10 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <RuntimeProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </RuntimeProvider>
-    </BrowserRouter>
+    <RuntimeProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </RuntimeProvider>
   </StrictMode>,
 );
